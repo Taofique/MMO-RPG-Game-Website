e337384ca72eaa5f883ffb1c8b26f9da12d6529f
Taofique <shornob8@gmail.com>
2025-07-13 17:15:33 +0600

    Add React 18 root rendering setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
-const root = document.getElementById("root");
+const root = document.getElementById("root")!;
 
-ReactDOM.createRoot(root!).render(
+createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
       <App />
